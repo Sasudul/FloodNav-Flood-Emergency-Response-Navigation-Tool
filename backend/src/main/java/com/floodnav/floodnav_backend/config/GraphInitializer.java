c0720115efd9e9c841fd06fa26c0e09a5ff08d62
Hirantha Rupasinghe <adriyallaka021085@gmail.com>
2026-02-19 14:04:45 +0530

    #4 Update GraphInitializer

--- backend/src/main/java/com/floodnav/floodnav_backend/config/GraphInitializer.java
+++ backend/src/main/java/com/floodnav/floodnav_backend/config/GraphInitializer.java
@@ -39,48 +39,54 @@ public class GraphInitializer implements CommandLineRunner {
         // Create ALL Nodes
         // =====================
 
-        Node kel01 = new Node("KEL01", 6.954833, 79.882278, "New Kelani River Bridge");
-        Node kel02 = new Node("KEL02", 6.983333, 79.883333, "Wattala Town Centre");
-        Node kel03 = new Node("KEL03", 6.968390, 79.892868, "Peliyagoda Interchange");
+        Node kel01 = new Node("KEL01", 6.984476, 79.888836, "New Kelani Bridge Interchange");
+        Node kel02 = new Node("KEL02", 6.959846, 79.886631, "77 Gongalegoda Banda Raja Mw");
+        Node kel03 = new Node("KEL03", 6.965422, 79.891414, "E03, Wattala");
+        Node kel04 = new Node("KEL04", 6.971362, 79.894026, "Thelangapatha - Wattala Flyover");
+        Node kel05 = new Node("KEL05", 6.976768, 79.897085, "E03, Dalugama");
+        Node kel06 = new Node("KEL06", 6.980812, 79.899510, "Kelanitissa Mawatha Rd");
+        Node kel07 = new Node("KEL07", 6.984736, 79.899206, "Station Rd Strat");
+        Node kel08 = new Node("KEL08", 6.988265, 79.900357, "Station Rd End");
+        Node kel09 = new Node("KEL09", 6.991057, 79.902507, "Welegoda, Wattala");
+        Node kel10 = new Node("KEL10", 7.000303, 79.904388, "Welikadamulla Rd");
 
 
         // =====================
         // Add ALL Nodes
         // =====================
 
-        graph.addNode(kel01); graph.addNode(kel02); graph.addNode(kel03);
-
+        graph.addNode(kel01); 
+        graph.addNode(kel02); 
+        graph.addNode(kel03);
+        graph.addNode(kel04);
+        graph.addNode(kel05);
+        graph.addNode(kel06);
+        graph.addNode(kel07);
+        graph.addNode(kel08);
+        graph.addNode(kel09);
+        graph.addNode(kel10);
 
         // =====================
         // Add Edges
         // =====================
 
 <<<<<<< HEAD
-        graph.addEdge("KEL01", "KEL03");
+        graph.addEdge("KEL01", "KEL02");
+        graph.addEdge("KEL02", "KEL03");
+        graph.addEdge("KEL03", "KEL04");
+        graph.addEdge("KEL04", "KEL05");
+        graph.addEdge("KEL05", "KEL06");
+        graph.addEdge("KEL06", "KEL07");
+        graph.addEdge("KEL07", "KEL08");
+        graph.addEdge("KEL08", "KEL09");
+        graph.addEdge("KEL09", "KEL10");
+
 =======
-        // --- Local east-side connector (Kiribathgoda ↔ Makola) ---
-        graph.addEdge("KEL12", "KEL13"); // Local connector road; split at Kiribathgoda junction
-        graph.addEdge("KEL15", "KEL16"); 
-        graph.addEdge("KEL17", "KEL18");
-        graph.addEdge("KEL17", "KEL19");
->>>>>>> b3378e3dcb603f20bec9db37f14f0e0602327704
 
 
-<<<<<<< HEAD
+
         System.out.println("✓ Loaded 31 nodes and road segments successfully");
 =======
-        graph.addEdge("KEL32", "KEL33");
-        graph.addEdge("KEL34", "KEL35");
-        graph.addEdge("KEL36", "KEL37");
-        graph.addEdge("KEL38", "KEL39");
-        graph.addEdge("KEL40", "KEL41");
-        graph.addEdge("KEL42", "KEL41");
-        graph.addEdge("KEL43", "KEL44");
-        graph.addEdge("KEL45", "KEL46");
-        graph.addEdge("KEL46", "KEL47");
-        graph.addEdge("KEL48", "KEL49");
-        graph.addEdge("KEL50", "KEL49");
-
 
         System.out.println("✓ Loaded 14 nodes and 13 road segments in Kelaniya area");
 >>>>>>> b3378e3dcb603f20bec9db37f14f0e0602327704
