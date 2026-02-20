863d3d95888daf1dc0ec3a7c3c2c717a6e65733e
Harsha20020703 <harshadeshappriya999@gmail.com>
2026-02-20 19:32:39 +0530

    Revert "change by Harsha"
    This reverts commit 6ce0d520cb3843af27d7f9eebf5bbb971e202c5d.

--- backend/src/main/java/com/floodnav/floodnav_backend/repository/SosRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.floodnav.floodnav_backend.repository;
-
-import com.floodnav.floodnav_backend.model.SosRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SosRequestRepository extends JpaRepository<SosRequest, Long> {
-}
--- backend/src/main/java/com/floodnav/floodnav_backend/service/RescueService.java
+++
@@ -1,8 +0,0 @@
-package com.floodnav.floodnav_backend.service;
-
-import com.floodnav.floodnav_backend.dto.RescueMissionDto;
-
-public interface RescueService {
-
-    RescueMissionDto getNextRescueMission();
-}
